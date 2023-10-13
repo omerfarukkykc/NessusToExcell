7c47359a4a3800e295f6e6d6958129e78043db3e
omerfarukkykc <54690370+omerfarukkykc@users.noreply.github.com>
2023-10-13 15:50:56 +0300

    Update NessusToXLSX.py

--- NessusToXLSX.py
+++ NessusToXLSX.py
@@ -1,8 +1,11 @@
 import xml.etree.ElementTree as ET
 from openpyxl import Workbook
+from tqdm import tqdm
 import argparse
 import os
+from googletrans import Translator
 wb = Workbook()
+translator = Translator()
 ws = wb.active
 def VulnerabilityIsExsist(vulnerability, unique_list):
     temp = ReportHostItem()
@@ -27,6 +30,7 @@ def VulnerabilityBasedReport(report):
                 vuln = Vulnerability(
                     plugin_name=vulnerability.plugin_name,
                     plugin_output=vulnerability.plugin_output,
+                    solution=vulnerability.solution,
                     description=vulnerability.description,
                     cve=vulnerability.cve,
                     severity=vulnerability.severity,
@@ -41,9 +45,10 @@ def VulnerabilityBasedReport(report):
 
 
 class ReportHostItem:
-    def __init__(self, plugin_name=None, plugin_output=None, description=None,cve=None,severity=None,severityID=None):
+    def __init__(self, plugin_name=None, plugin_output=None, solution = None,description=None,cve=None,severity=None,severityID=None):
         self.plugin_name = plugin_name
         self.plugin_output = plugin_output
+        self.solution = solution
         self.description = description
         self.cve = cve
         self.severity = severity
@@ -59,10 +64,11 @@ class ReportHost:
         for item in self.reportHostItems:
             item.print()
 class Vulnerability:
-    def __init__(self, plugin_name, plugin_output, description,cve,severity,severityID,ipList):
+    def __init__(self, plugin_name, plugin_output, solution,description,cve,severity,severityID,ipList):
         self.ipList = ipList
         self.plugin_name = plugin_name
         self.plugin_output = plugin_output
+        self.solution = solution
         self.description = description
         self.cve = cve
         self.severity = severity
@@ -78,6 +84,7 @@ def ParseReportHostItem(reportHostItem):
     plugin_name = ""
     plugin_output = ""
     description = ""
+    solution = ""
     cve = ""
     for Item in reportHostItem:
         if Item.tag == "plugin_name":
@@ -86,15 +93,28 @@ def ParseReportHostItem(reportHostItem):
             plugin_output = Item.text
         if Item.tag == "description":
             description = Item.text
+        if Item.tag == "solution":
+            solution = Item.text
         if Item.tag == "cve":
-            cve= Item.text
+            if cve == "":
+                cve = Item.text
+            else:
+                cve += ", " + Item.text
     if plugin_name =="" and plugin_output=="" and description == "" and cve =="":
         return None
-    return ReportHostItem(plugin_name,plugin_output,description,cve,GetSeverityString(severityID),int(severityID))
+    return ReportHostItem(
+        plugin_name=plugin_name,
+        plugin_output=plugin_output,
+        solution=solution,
+        description=description,
+        cve=cve,
+        severity=GetSeverityString(severityID),
+        severityID=int(severityID)
+        )
+
 def ParseReportHost(reportHost):
     if reportHost.tag != "ReportHost":
         return
-
     ip = reportHost.get("name")
     reportItemArray = []
     for ReportItem in reportHost.findall("ReportItem"):
@@ -136,7 +156,7 @@ def check_input_file(value):
     if not os.path.exists(value):
         raise argparse.ArgumentTypeError(f"Input file '{value}' does not exist")
     return value
-def main(input_file, output_file, severity, cve,IpBased):
+def main(input_file, output_file, severity, cve,IpBased,translate):
     print(f"Input file: {input_file}")
     print(f"Output file: {output_file}")
     print(f"Severity: {severity}")
@@ -148,33 +168,41 @@ def main(input_file, output_file, severity, cve,IpBased):
         row = 2
         if IpBased == False:
             vreport = VulnerabilityBasedReport(report)
-            for vuln in vreport:
+            for vuln in tqdm(vreport, "Progress: "):
                 if vuln.severityID < severity:
                     continue
                 if cve and vuln.cve == "":
                     continue
                 ws.cell(row=row, column=1, value=vuln.plugin_name)
-                ws.cell(row=row, column=2, value=vuln.plugin_output)
-                ws.cell(row=row, column=3, value=vuln.description)
-                ws.cell(row=row, column=4, value=vuln.cve)
-                ws.cell(row=row, column=5, value=vuln.severity)
-                ws.cell(row=row, column=6, value=', '.join(vuln.ipList))
+                ws.cell(row=row, column=1, value=translator.translate(vuln.plugin_name, dest=translate).text)
+                ws.cell(row=row, column=2, value=vuln.description)
+                ws.cell(row=row, column=3, value=translator.translate(vuln.description, dest=translate).text)
+                ws.cell(row=row, column=4, value=vuln.solution)
+                ws.cell(row=row, column=5, value=translator.translate(vuln.solution, dest=translate).text)
+                ws.cell(row=row, column=6, value=vuln.cve)
+                ws.cell(row=row, column=7, value=translator.translate(vuln.severity, dest=translate).text)
+                ws.cell(row=row, column=8, value=', '.join(vuln.ipList))
+                ws.cell(row=row, column=9, value=vuln.plugin_output)
                 row = row + 1
 
+
         else:
-            for reportHostsIdx, reportHosts in enumerate(report):
-                for hostItemIdx,hostItem in enumerate(reportHosts.reportHostItems):
+            for reportHosts in tqdm(report,"Host Porgress: "):
+                for hostItem in tqdm(reportHosts.reportHostItems,"Host Vulns: "):
                     if hostItem.severityID < severity:
                         continue
                     if cve and hostItem.cve == "":
                         continue
-                    
                     ws.cell(row=row, column=1, value=reportHosts.ip)
                     ws.cell(row=row, column=2, value=hostItem.plugin_name)
-                    ws.cell(row=row, column=3, value=hostItem.plugin_output)
-                    ws.cell(row=row, column=4, value=hostItem.description)
-                    ws.cell(row=row, column=5, value=hostItem.cve)
-                    ws.cell(row=row, column=6, value=hostItem.severity)
+                    ws.cell(row=row, column=2, value=translator.translate(hostItem.plugin_name, dest=translate).text)
+                    ws.cell(row=row, column=3, value=hostItem.description)
+                    ws.cell(row=row, column=4, value=translator.translate(hostItem.description, dest=translate).text)
+                    ws.cell(row=row, column=5, value=hostItem.solution)
+                    ws.cell(row=row, column=6, value=translator.translate(hostItem.solution, dest=translate).text)
+                    ws.cell(row=row, column=8, value=translator.translate(hostItem.severity, dest=translate).text)
+                    ws.cell(row=row, column=7, value=hostItem.cve)
+                    ws.cell(row=row, column=9, value=hostItem.plugin_output)
                     row = row + 1
     
     wb.save(output_file)
@@ -182,12 +210,13 @@ def main(input_file, output_file, severity, cve,IpBased):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Enjoy brother !!")
-    parser.add_argument("-i", dest="input_file", type=check_input_file, default="scan.nessus", help="Path to the input file")
-    parser.add_argument("-o", dest="output_file", default="output.xlsx", help="Path to the output file")
-    parser.add_argument("-s", dest="severity", type=check_severity, default=2, choices=[0, 1, 2, 3, 4], 
-                        help="(Default 2) Minimum severity level: 0 (informational), 1 (low), 2 (medium), 3 (high), 4 (critical)")
-    parser.add_argument("--cve", action="store_true", help="Just Include if has CVE information")
-    parser.add_argument("-IpBased", action="store_true", help="IP based output")
+    parser.add_argument("--input","-i", dest="input_file", type=check_input_file, default="scan.nessus", help="Path to the input file")
+    parser.add_argument("--output","-o", dest="output_file", default="output.xlsx", help="Path to the output file")
+    parser.add_argument("--severity","-s", dest="severity", type=check_severity, default=2, choices=[0, 1, 2, 3, 4], help="(Default 2) Minimum severity level: 0 (informational), 1 (low), 2 (medium), 3 (high), 4 (critical)")
+    parser.add_argument("--cve","-c", action="store_true", help="Just Include if has CVE information")
+    parser.add_argument("--translate","-t",default="tr", help="Just Include if has CVE information")
+    parser.add_argument("-IpBased","-ib",action="store_true", help="IP based output")
     args = parser.parse_args()
 
-    main(args.input_file, args.output_file, args.severity, args.cve,args.IpBased)
+    main(args.input_file, args.output_file, args.severity, args.cve,args.IpBased,args.translate)
+
